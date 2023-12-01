e70181ce1f30511a4afdfb2e16e9fdf2c1607df2
Eunji <libera0129@gmail.com>
2023-12-01 20:54:05 +0900

    JS: Home

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// 페이지를 아래로 스크롤 시 Header에 다크 스타일링 적용
+// 아래로 스크롤 시 Header에 다크 스타일링 적용
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -7,4 +7,11 @@ document.addEventListener('scroll', () => {
   } else {
     header.classList.remove('header--dark');
   }
+});
+
+// 홈 섹션을 아래로 스크롤 시 투명하게 처리.
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+  home.style.opacity = `${0.1 + (homeHeight - scrollY) / homeHeight}`;
 });
